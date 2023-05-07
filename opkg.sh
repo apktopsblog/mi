3260b918e863c58c070e4076e40816d59d3aec3b
apktopsblog <35693587+apktopsblog@users.noreply.github.com>
2023-05-07 18:05:15 +0330

    Create opkg.sh

---
+++ opkg.sh
@@ -0,0 +1,10 @@
+opkg install ipset
+opkg install ipt2socks 
+opkg install iptables 
+opkg install iptables-legacy 
+opkg install iptables-mod-conntrack-extra 
+opkg install iptables-mod-iprange 
+opkg install iptables-mod-socket 
+opkg install iptables-mod-tproxy 
+opkg install kmod-ipt-nat 
+opkg install dnsmasq-full
