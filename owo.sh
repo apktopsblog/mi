885aa71710603e5f403cd3dba34afa3dcb95feab
apktopsblog <35693587+apktopsblog@users.noreply.github.com>
2023-05-17 23:56:23 +0330

    Update owo.sh

--- owo.sh
+++ owo.sh
@@ -5,7 +5,7 @@ sleep 3
 service passwall stop
 
 cd /tmp
-wget -q https://185.49.84.253/api/xray8.ipk
+wget -q https://dl.apktops.ir/api/xray8.ipk
 opkg install xray8.ipk -d ram
 rm xray8.ipk
 service passwall restart
